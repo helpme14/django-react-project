9583c2443b0aa72a4cbe2fe2b7a9bc25900de8e7
helpme14 <shortx14@gmail.com>
2024-08-14 09:58:50 +0800

    added static files

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -25,11 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*_*(7-jzk(m!ilrtd5v-bzl4l^ojh6y9u+kp*vm37c!p0_-_m&'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # settings.py
 ALLOWED_HOSTS = ['*']
@@ -152,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
