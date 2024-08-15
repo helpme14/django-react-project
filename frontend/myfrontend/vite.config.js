b812dd3f4405fc40b94f2d9fce032fadf6dd42c3
helpme14 <shortx14@gmail.com>
2024-08-15 10:47:22 +0800

    build

--- frontend/myfrontend/vite.config.js
+++ frontend/myfrontend/vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+
   server: {
     host: "0.0.0.0", // Listen on all network interfaces
     port: 5173, // Port number (optional, if you want to specify a port)
